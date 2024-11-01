c65723d3b0afdb940fa4c9b2a5d4073583e52ec7
jayedHoshen <jayedkn64@gmail.com>
2024-11-01 23:43:13 +0600

    week-1: day-6

--- Week_1/Day_6/Not_Found.cpp
+++ Week_1/Day_6/Not_Found.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     string s;
     cin >> s;
 
     int sz = s.size();
-    int cnt[28];
+    int cnt[27];
+
     for(int i = 0; i < 26; i++) cnt[i] = 0;
 
     for(int i = 0; i < sz; i++) {
-        
+        cnt[s['a' - i]]++;
     }
 
     for(int i = 0; i < 26; i++) cout << cnt[i] << ' ';
