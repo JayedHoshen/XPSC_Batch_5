d1082adc2b0b9c51e6e5b64b27ef738681956a78
jayedHoshen <jayedkn64@gmail.com>
2024-11-24 23:45:10 +0600

    week_5: day_1

---
+++ Week_5/Day_1/Number_of_Equal.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1; 
+    cin >> tc;
+
+    while(tc--) {
+        
+    }
+
+    return 0;
+}
