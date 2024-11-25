472a842106f93c0b49aa4e75424c6e3df58f22e5
jayedHoshen <jayedkn64@gmail.com>
2024-11-25 23:57:30 +0600

    week_5: day_2

--- Week_5/Day_1/Number_of_Equal.cpp
+++ Week_5/Day_1/Number_of_Equal.cpp
@@ -6,12 +6,25 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int tc = 1; 
-    cin >> tc;
+    int n, m;
+    cin >> n >> m;
 
-    while(tc--) {
-        
+    vector <int> a(n), b(m);
+
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < m; i++) cin >> b[i];
+
+    int l = 0, r = 0;
+    long long int ans = 0;
+    while(l < n && r < m) {
+        int cnt1 = 0, cnt2 = 0, cur = a[l];
+        while(l < n && cur == a[l]) cnt1++, l++;
+        while(r < m && cur > b[r]) r++;
+        while(r < m && b[r] == cur) cnt2++, r++;
+        ans += (1LL * cnt1 * cnt2);
     }
+
+    cout << ans << '\n';
 
     return 0;
 }
