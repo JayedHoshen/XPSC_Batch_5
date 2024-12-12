2fec0e42697655d2f436bf3f51883b133a6098be
jayedHoshen <jayedkn64@gmail.com>
2024-12-12 22:34:17 +0600

    week_7: day_5

---
+++ Week_7/Day_5/Money_Trees.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1; 
+    cin >> tc;
+
+    while(tc--) {
+        
+    }
+
+    return 0;
+}
