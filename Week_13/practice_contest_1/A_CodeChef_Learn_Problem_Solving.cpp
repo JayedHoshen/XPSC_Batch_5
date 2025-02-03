f8f6a076d27aac1eca498f1e6b5dbcdf24f92fe7
jayedHoshen <jayedkn64@gmail.com>
2025-02-03 21:19:25 +0600

    week_12: practice_contest_1

---
+++ Week_13/practice_contest_1/A_CodeChef_Learn_Problem_Solving.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define FIO() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fi0(n) for(int i = 0; i < n; i++)
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    FIO(); 
+
+    int n; cin >> n;
+    cout << n*2 << '\n';
+
+    return 0;
+}
